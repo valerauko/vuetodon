4ceb428f80d5d31b9100446bbc82709705d0f445
Unknown <valerauko@gmail.com>
2018-01-04 17:22:40 +0900

    Add Card loading

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="card">
+    <a :href="card.url" rel="nofollow noopener" target="_blank">
+      <img v-if="card.image" :src="card.image" />
+      <h1>{{ card.title }}</h1>
+      <p>{{ card.description }}</p>
+    </a>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: ['card']
+}
+</script>
+
+<style scoped>
+section.card {
+  width: 70%;
+  margin: .5em auto;
+
+}
+</style>
