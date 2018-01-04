5b6a11afe8fb63b7016915e5bf1c8b968e9763e3
Unknown <valerauko@gmail.com>
2018-01-04 22:29:06 +0900

    Login/Logout components

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,23 @@
 <template>
-  <div>
+  <modal :show="show">
     <h2>Log in!</h2>
     <form>
-      <input type="text" v-model="instance" placeholder="Your instance" />
+      <input type="text" placeholder="Your instance"
+             v-model="instance" @focus="focusInstance" @blur="blurInstance" />
       <input type="text" v-model="username" placeholder="Username" />
       <input type="password" v-model="password" placeholder="Password" />
-      <button @click="login">Toot!</button>
+      <button @click="login">Log in!</button>
     </form>
-  </div>
+  </modal>
 </template>
 
 <script>
+import Modal from '@/components/Modal'
 export default {
   name: 'Login',
+  components: {
+    Modal
+  },
   data () {
     return {
       instance: '',
@@ -20,11 +25,23 @@ export default {
       password: ''
     }
   },
+  props: ['show'],
   methods: {
+    close () {
+      this.$emit('close')
+    },
+    focusInstance () {
+      this.instance = this.instance || 'https://'
+    },
+    blurInstance () {
+      if (this.instance.match(/^https?:\/\/$/ui)) {
+        this.instance = ''
+      }
+    },
     login () {
-      var known = this.store.out('known_instances')
-      if (known == null || known[new URL(this.instance).host] == null) {
-        this.registerWithInstance(this.instance).then(this.attemptLogin)
+      let uri = new URL(this.instance)
+      if (this.known()[uri.host] == null) {
+        this.registerWithInstance(uri).then(this.attemptLogin)
       } else {
         this.attemptLogin()
       }
@@ -37,32 +54,41 @@ export default {
         localStorage.setItem(key, storable)
         return storable
       },
-      out: key => JSON.parse(localStorage.getItem(key))
+      out: key => {
+        let raw = localStorage.getItem(key)
+        return raw !== 'undefined' ? JSON.parse(raw) : {}
+      }
+    }
+    this.known = (val = null) => {
+      if (val == null) {
+        return this.store.out('known_instances')
+      } else {
+        return this.store.in('known_instances', val)
+      }
     }
     this.registerWithInstance = uri => {
-      uri = new URL(uri)
       var target = uri.origin
-      this.$http.post(target + '/api/v1/apps', {
-        client_name: 'vuetodon',
+      return this.$http.post(target + '/api/v1/apps', {
+        client_name: 'Vuetodon',
         redirect_uris: 'http://localhost:3000',
         scopes: ['read', 'write', 'follow'].join(' '),
         website: 'http://valerauko.net'
       }).then(response => {
-        var known = this.store.out('known_instances') || {}
+        var known = this.known()
         known[uri.host] = {
           host: uri.origin,
           client_id: response.body.client_id,
           client_secret: response.body.client_secret
         }
-        this.store.in('known_instances', known)
+        this.known(known)
         console.log('App registration successful')
       }, response => {
-        console.log('App registration failed')
+        alert('App registration failed')
         console.log(response)
       })
     }
     this.attemptLogin = () => {
-      var instance = this.store.out('known_instances')[new URL(this.instance).host]
+      let instance = this.known()[new URL(this.instance).host]
       this.$http.post(instance.host + '/oauth/token', {
         client_id: instance.client_id,
         client_secret: instance.client_secret,
@@ -72,9 +98,13 @@ export default {
         scope: ['read', 'write', 'follow'].join(' ')
       }).then(response => {
         console.log('Login successful')
-        console.log(response.body)
+        this.store.in('instance', instance.host)
+        this.store.in('token', response.body.access_token)
+        this.$http.headers.common['Authorization'] =
+          'Bearer ' + response.body.access_token
+        this.close()
       }, response => {
-        console.log('Login failed')
+        alert('Login failed')
         console.log(response)
       })
     }
@@ -90,7 +120,7 @@ input {
   border-radius: 5px;
   margin: 0.5em auto;
   padding: 1em;
-  width: 30vw;
+  width: inherit;
   font: inherit;
   color: #fff;
 }
